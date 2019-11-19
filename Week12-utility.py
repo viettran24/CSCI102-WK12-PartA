3bd823f8aeaad791477bbf0a76920a8ce6311ca5
Your Name <you@example.com>
2019-11-19 04:39:44 -0700

    function 6 finished

--- Week12-utility.py
+++ Week12-utility.py
@@ -29,16 +29,30 @@ def FindWordCount(filename,string):
     my_string = my_string.count(string)
     return my_string
 
+players = ["Mary", "Cody", "Joe", "Jill", "Xai", "Bodo"]
+scores = [5, 8, 10, 6, 10, 4]
+
 def ScoreFinder(players,scores,name):
     i = 0
+    clean_list = []
     for x in players:
+        clean_list.append(x.lower())
         if x != name:
             i = i + 1
         else:
             return players[i], "has a score of", scores[i]
-    if a not in players:
+    if name not in players:
         return "player not found"
 
 
+def Union(list1,list2):
+    new_list = []
+    for x in list1:
+        new_list.append(x)
+    for x in list2:
+        new_list.append(x)
+    return new_list
+
+PrintOutput(Union(players,scores))
 
 
