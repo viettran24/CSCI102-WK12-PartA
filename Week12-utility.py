e749985c0bf9d0669d1c1d26b3ed34b0b3138796
Your Name <you@example.com>
2019-11-19 02:51:09 -0700

    forgot to commit at file start

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,22 +1,3 @@
 #Viet Tran
 #Section B
 #Week 12 - Part A
-
-def PrintOutput(string):
-    print("OUTPUT:", string)
-
-def LoadFile(filename):
-    with open(filename) as file:
-        elementList = []
-        for line in file:
-            elementList.append(line)
-    return elementList
-    
-def UpdateString(string1,string2,index):
-    letterList = []
-    for char in string1:
-        letterList.append(char)
-        letterList[index] = string2
-    return letterList
-
-PrintOutput(UpdateString("Hello World","a",0))
