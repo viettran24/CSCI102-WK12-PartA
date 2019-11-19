4d91fe38520d88e9fd1c075c1b68b5cc47e746ba
Your Name <you@example.com>
2019-11-19 03:05:16 -0700

    function 3 finished

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,3 +1,23 @@
 #Viet Tran
 #Section B
 #Week 12 - Part A
+import re
+import string
+
+def PrintOutput(string):
+    print("OUTPUT:", string)
+
+def LoadFile(filename):
+    with open(filename) as file:
+        elementList = []
+        for line in file:
+            elementList.append(line)
+    return elementList
+
+def UpdateString(string1,string2,index):
+    letterList = []
+    for char in string1:
+        letterList.append(char)
+    letterList[index] = string2
+    return letterList
+PrintOutput(UpdateString("Hello World","a",6))
